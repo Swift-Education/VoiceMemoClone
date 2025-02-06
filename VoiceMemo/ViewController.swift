313c6b06d8f0e97a14a98b92eec90c67ce079c02
kangddong <rkdehddud96@naver.com>
2025-02-06 23:44:37 +0900

    first commit

---
+++ VoiceMemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VoiceMemo
+//
+//  Created by 강동영 on 2/6/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
